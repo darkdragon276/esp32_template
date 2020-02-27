282048df204cffa9634cc3e66c415698c76a3db1
chuhainam <chuhai.nam1997@gmail.com>
2020-02-27 10:00:37 +0700

    update component

--- main/component.mk
+++ main/component.mk
@@ -3,5 +3,7 @@
 #
 # (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
 
-# COMPONENT_ADD_INCLUDEDIRS := ./app .
-# COMPONENT_SRCDIRS :=  ./app .
+COMPONENT_ADD_INCLUDEDIRS := ./app .
+COMPONENT_SRCDIRS :=  ./app .
+COMPONENT_EXTRA_INCLUDES := \
+	$(IDF_PATH)/components/freertos/include/freertos \
