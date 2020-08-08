303d0e5bbddb33efc14faf61151961a41a78eca3
quocbao74 <quocbao.study@gmail.com>
2020-08-08 12:28:32 +0700

    upload config.c

--- main/config.c
+++ main/config.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "freertos/FreeRTOS.h"
-//#include "freertos/task.h"
+#include "freertos/task.h"
 #include "freertos/queue.h"
 #include "driver/gpio.h" 
 
