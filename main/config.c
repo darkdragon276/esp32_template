b538ec0a8e6d1579e9dd6c1c4ba1304a872cd936
quocbao74 <quocbao.study@gmail.com>
2020-08-10 12:58:29 +0700

    upload config.c

--- main/config.c
+++ main/config.c
@@ -5,41 +5,20 @@
 #include "freertos/task.h"
 #include "freertos/queue.h"
 #include "driver/gpio.h" 
+#include "define.h"
+static xQueueHandle dd_gpio_Queue_v = NULL; // declare variable to receive the value of a func pointer
 
-#define DD_GPIO_OUTPUT_IO_1 1;
-#define DD_GPIO_OUTPUT_IO_2 2;
-#define DD_GPIO_OUTPUT_IO_3 3;
-#define DD_GPIO_OUTPUT_IO_4 4;
-#define DD_GPIO_OUTPUT_IO_5 5;
-
-static xQueueHandle gpio_evt_queue = NULL; // declare variable to receive the value of 
-
-static void IRAM_ATTR button_isr_handler(void* value_of_intr_gpio){
-    uint32_t gpio_num = (uint32_t) value_of_intr_gpio; // add value of gpio to a variable.
-    xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL); // function to send value of gpio out.
+static void IRAM_ATTR dd_gpio_isr_handler(void* intr_value_vpt){
+    uint32_t gpio_num = (uint32_t) intr_value_vpt; // add value of gpio to a variable.
+    xQueueSendFromISR(dd_gpio_Queue_v, &gpio_num, NULL); // function to send value of gpio out.
 }
-    static void gpio_task(void* arg)
-{
-    uint32_t io_num;
-    for(;;) {
-        if(xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
-            printf("GPIO[%d] intr, val: %d\n", io_num, gpio_get_level(io_num));
-        }
-    }
-}
-void intr_config(int a){
-    dd_gpio_config_t button_config;
-    button_config.pin_bit_mask = BIT0; // declare GPIO 0 for interrupt signal input 
-    button_config.mode = GPIO_MODE_INPUT; // set up GPIO is input
-    button_config.pull_up_en = GPIO_PULLUP_ENABLE; // set volrage for gpio 0 to high
-    button_config.intr_type = GPIO_INTR_POSEDGE; // set interrupt when the signal is rising. 0 -> 1
-    gpio_config(&button_config); // get the value of button signal
-    gpio_set_intr_type(GPIO_NUM_0,GPIO_INTR_POSEDGE); // set interrupt type (gpio 0, rising)
+void dd_gpio_Config_func(dd_gpio_Config_tst gpioConfig){
+    gpio_config((gpio_config_t*)&gpioConfig); // get the value of button signal
+    gpio_set_intr_type(gpioConfig.ioNum_en,dd_gpio_intr_posEdge_en); // set interrupt type (gpio 0, rising)
     gpio_install_isr_service(ESP_INTR_FLAG_EDGE); // interrupt whenever the signal is asserted
-    gpio_isr_handle_add(GPIO_NUM_0, button_isr_handler, (void*) GPIO_NUM_0) 
-  
+    gpio_isr_handle_add(gpioConfig.ioNum_en, gpioConfig.isrHandler_func, gpioConfig.isrArgument);
+    dd_gpio_Queue_v = xQueueCreate(gpioConfig.queueLenght_cu32, sizeof(uint32_t));
 }
 int app_main(void){
-    gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
 
 }
